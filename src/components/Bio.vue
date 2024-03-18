d4277dbe03639a6180f0d4ea45a813858930f458
P.E. Butler III <p.e.butler3@gmail.com>
2024-03-18 14:31:44 -0600

    feat: updated Bio

--- src/components/Bio.vue
+++ src/components/Bio.vue
@@ -1,12 +1,10 @@
 <template>
   <div class="wrapper">
     <section class="bio">
-      <h2>About:</h2>
-      <p>I am a Senior Software Engineer @ Upland with a focus on the front-end.</p>
-      <p>Living in <b>Denver, Colorado</b> with
-        <a class="bio__repos-link" :href="publicReposUrl">{{ publicRepos }}</a>
-        <b>Public Repos</b>
-      </p>
+      <h2>About</h2>
+      <p>With over 11 years of experience, my unwavering commitment to continuous learning has positioned me as a consistent and innovative member of numerous teams. From fast-growing startups to social media companies and some of the world's most recognizable brands, I've successfully navigated varied environments, always hitting the ground running.</p>
+      <p><strong>Current Position: </strong>I am a Senior Software Engineer @ Upland with a focus on the front-end.</p>
+      <p>Living in <strong>Denver, Colorado</strong></p>
     </section>
     <slot />
   </div>
@@ -22,6 +20,7 @@ h2 {
 
 .wrapper {
   display: flex;
+  flex: 1;
   flex-direction: column;
   padding: 2rem;
 }
